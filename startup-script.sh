da56204d1ac11a0ad2b377308a791b6fbd5e6dfe
lilliekhogg <lilliekhogg@gmail.com>
2017-11-30 12:16:12 +0000

    Create startup-script.sh

---
+++ startup-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+//install all dependancies needed for task
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
+sudo apt-get install -y nodejs;
+sudo apt-get install git;
+
+
+
+
+//installing the clocoss-master-worker
+
+git clone https://github.com/portsoc/clocoss-master-worker;
+cd clocoss-master-worker;
+npm install;
+
+
+secretKey=`curl -s -H "Metadata-Flavor: Google"  \
+"http://metadata.google.internal/computeMetadata/v1/instance/attributes/secret"`;
+serverip=`curl -s -H "Metadata-Flavor: Google"  \
+"http://metadata.google.internal/computeMetadata/v1/instance/attributes/serverip"`;
+
+
+//run the client
+npm run client $secretKey; $serverip:8080;
