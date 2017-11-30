2609dee5a25ffb34b3232fce3b76090dfd77fd84
lilliekhogg <lilliekhogg@gmail.com>
2017-11-30 13:51:09 +0000

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -3,7 +3,7 @@
 #install all dependancies needed for task
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
 sudo apt-get install -y nodejs;
-sudo apt-get install git;
+sudo apt-get install -y git;
 
 
 
