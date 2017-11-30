ac0cf5e71785d086dc13c4df85e23c221317d8f8
lilliekhogg <lilliekhogg@gmail.com>
2017-11-30 12:15:47 +0000

    Create task1.sh

---
+++ task1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+N=$1;
+
+secretKey=`openssl rand -base64 32`;
+workerName="lillie-worker";
+
+
+//installing dependencies 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash;
+sudo apt-get -q install nodejs;
+sudo apt-get -q install git;
+
+//clone master-worker github
+git clone https://github.com/portsoc/clocoss-master-worker;
+cd clocoss-master-worker;
+
+npm install;
+
+
+//create the instances
+
+for i in `seq 1 $N`;
+do
+        gcloud compute instances create "$workerName"-"$i" \
+        --machine-type f1-micro \
+        --tags http-server,https-server \
+        --metadata secret=$secretKey,serverip=`curl -s -H "Metadata-Flavor: Google" \ "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"` \
+        --metadata-from-file \
+          startup-script=../startup-script.sh \
+        --quiet;
+done;
+
+//running server
+npm run server $secretKey;
+
+
+//remove workers
+cd..;
+sudo rm clocoss-master-worker -r;
+
+for i in `seq 1 $N`;
+do
+        gcloud compute instances delete "$workerName"-"$i" --quiet;
+done;
