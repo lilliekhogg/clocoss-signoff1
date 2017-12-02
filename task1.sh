d5ac103763d39117c36948b30cb5f196fde352b1
lilliekhogg <lilliekhogg@gmail.com>
2017-12-02 20:23:20 +0000

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -28,9 +28,10 @@ do
         gcloud compute instances create "$workerName"-"$i" \
         --machine-type f1-micro \
         --tags http-server,https-server \
-        --metadata secret=$secretKey,serverip=`curl -s -H "Metadata-Flavor: Google" \ "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"` \
+        --metadata secret=$secretKey,serverip=`curl -s -H "Metadata-Flavor: Google" \
+                                               "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"` \
         --metadata-from-file \
-        startup-script=../startup-script.sh\
+        startup-script=../startup-script.sh \
         --quiet;
 done;
 
