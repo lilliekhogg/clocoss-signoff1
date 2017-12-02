c3fb8e959e86470589bb14444532cc9383090780
lilliekhogg <lilliekhogg@gmail.com>
2017-12-02 23:02:12 +0000

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -2,10 +2,10 @@
 
 
 N=$1;
-echo "The number of vms created will be $N"
+echo "The number of vms created will be $N";
 
 secretKey=`openssl rand -base64 32`;
-echo "the secret key is $secretKey"
+echo "the secret key is $secretKey";
 workerName="lillie-worker";
 
 
@@ -29,7 +29,7 @@ do
         --machine-type f1-micro \
         --tags http-server,https-server \
         --metadata secret=$secretKey,serverip=`curl -s -H "Metadata-Flavor: Google" \
-                                               "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"` \
+                                               "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"'; \
         --metadata-from-file \
                 startup-script="../startup-script.sh" \
         --quiet;
