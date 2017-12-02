08e658c803ea04c4b4bef8680b186827ab7e36df
lilliekhogg <lilliekhogg@gmail.com>
2017-12-02 20:45:38 +0000

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -30,7 +30,8 @@ do
         --tags http-server,https-server \
         --metadata secret=$secretKey,serverip=`curl -s -H "Metadata-Flavor: Google" \
                                                "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip"` \
-        --metadata-from-file startup-script=startup-script.sh \
+        --metadata-from-file \
+        startup-script=startup-script.sh \
         --quiet;
 done;
 
